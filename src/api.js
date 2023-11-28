fe49bc6ddafb25636f3e2ad339ccede480331019
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-11-28 13:28:17 -0300

    Alterando url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:7119/api',
-    //baseURL: 'https://apisunsale.azurewebsites.net/api',
+    //baseURL: 'https://localhost:7119/api',
+    baseURL: 'https://apisunsale.azurewebsites.net/api',
     headers: {
         'Content-Type': 'application/json',
     },
